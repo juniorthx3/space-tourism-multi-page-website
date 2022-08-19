d25683187325b6cc1eb4ea4dc4268f7df1b07530
Junior MILIEN <juniorthx@gmail.com>
2022-08-18 22:29:11 -0400

    Improve homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section className="home">
        <div className="h-full flex flex-col items-center justify-center md:flex-row md:justify-evenly" style={{width:"min(100% - 4rem, 80em)", marginInline:"auto"}}>
-          <div className="flex flex-col items-center justify-evenly md:flex-row">
+          <div className="mt-28 flex flex-col items-center justify-evenly md:flex-row md:ml-32">
               <div className='space-y-4 mx-auto text-center md:text-left'>
                   <h2 className='text-xl uppercase tracking-widest'>So, you want to travel to</h2>
                   <h1 className='text-8xl  uppercase font-light tracking-widest  md:text-9xl'>Space</h1>
@@ -14,7 +14,7 @@ const Home = () => {
                       because we'll give you a truly out of this world experience!
                   </p>
               </div>
-              <div className='w-1/2 mt-24'>
+              <div className='mt-20 md:w-1/2'>
                 <Link to="/destination"><button className='bg-white text-black tracking-widest text-lg font-medium uppercase rounded-full w-44 h-44'>Explore</button></Link>
               </div>
           </div>
