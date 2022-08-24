024949712c45f84a18dc308b9a245a1200f8a5db
Junior MILIEN <juniorthx@gmail.com>
2022-08-24 11:34:09 -0800

    Finish Destination Page

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -9,13 +9,15 @@ const Crew = () => {
 
   return (
     <section className="crewBackground crew">
-      <h2 className="h2 md:mb-10"><span className="span">02</span>Meet your crew</h2>
-       <div className='containerBox'>
+       <div className='containerBox md:mt-36'>
           <div className='text-center md:text-left'>
+          {/* *************************MOBILE DESIGN************************* */}
+            {/* Images of Crew in mobile size */}
             <div className='block md:hidden'>   
               <img src={images.png} alt={name} title={name} className="w-1/2 mx-auto" />
               <hr className="text-specialBlue border-b-1 border-gray-500" />
             </div>
+            {/* Navigation in mobile size for Crew Team */}
             <div className='pt-5 flex items-center justify-center md:pt-0 md:hidden md:fixed md:bottom-10'>
                 <ul className="flex items-center justify-center mb-4 md:justify-start md:mb-10">
                     {records.map((menu, index) => (
@@ -29,16 +31,20 @@ const Crew = () => {
                     ))}
                 </ul>
              </div>
-            <div className='mr-5 md:mr-0 md:ml-16'>
+             {/* *****************************************************************/}
+
+             {/* ***************************PC DESIGN*****************************/}
+            <div className='text-center md:text-left'>
+              <h2 className="h2 md:mb-28"><span className="span">02</span>Meet your crew</h2>
               <h3 className='font-bellefair not-italic font-normal text-xl leading-9 uppercase text-white mix-blend-normal opacity-50 md:text-3xl'>{role}</h3>
               <h2 className="font-bellefair not-italic font-normal text-2xl uppercase text-white md:mt-4 mb-5 md:text-5xl">{name}</h2>
               <p className='mb-10 w-3/4 mx-auto font-barlow not-italic font-normal text-sm leading-6 text-specialBlue md:mx-0 md:w-3/5 md:mb-0'>{bio}</p>
               <div className='hidden md:block md:mt-20 '>
                 <ul className="flex items-center justify-center md:justify-start mb-10">
                     {records.map((menu, index) => (
-                      <li key={index} className="mr-7 md:mr-5">
+                      <li key={index} className="mr-7 md:mr-5 text-gray-500">
                         <button onClick={() => setIndexx(index)} 
-                                className={`${index === indexx && "border-white text-specialBlue"}`}
+                                className={`${index === indexx && "border-white text-white"}`}
                         >
                           <FaCircle size={10}/>
                         </button>
@@ -48,9 +54,10 @@ const Crew = () => {
               </div>
            </div>
           </div>
-          <div className='hidden md:flex items-end md:pt-16'>   
-              <img src={images.png} alt={name} title={name} className="w-1/2 md:w-3/5 mx-auto md:mt-5;" />
+          <div className='hidden md:block'>   
+              <img src={images.png} alt={name} title={name} className="px-24 mx-auto"/>
           </div>
+          {/* *****************************************************************/}
       </div>
     </section>
   )
--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -8,37 +8,37 @@ const Destination = () => {
 
   return (
     <section className="destinationBackground destination">
-      <h2 className="h2"><span className="span">01</span>Pick your destination</h2>
-      <div className='containerBox'>
-          <div className='flex items-center text-center md:text-left'>
-            <img src={images.png} alt={name} title={name} className="img" />
-          </div>
-          <div className='infoBox'>  
-              <ul className="flex items-center justify-center mb-8 md:justify-start">
-                  {records.map((menu, index) => (
-                    <li key={index} className="mr-7 md:mr-8">
-                      <button onClick={() => setIndexx(index)} 
-                              className={`font-barlowCondensed not-italic font-normal leading-4 text-specialBlue uppercase text-md tracking-widest pb-2 focus:text-white md:text-md ${index === indexx && "border-b-2 border-white"}`}
-                      >
-                        {menu.name}
-                      </button>
-                    </li>
-                  ))}
-              </ul>
-              <h2 className="title">{name}</h2>
-              <p className= "description">{description}</p>
-              <ul className="stats-container">
-                <li className="stats-design">
-                  Avg. Distance
-                  <span className="stats-results">{distance}</span>
-                </li>
-                <li className="stats-design">
-                  Est. travel time
-                  <span className="stats-results">{travel}</span>
-                </li>
-              </ul>
-          </div>
-      </div>
+       <div className='mt-28 md:mt-36'>
+            <h2 className="h2 mb-3 md:mb-0 md:mx-32"><span className="span">01</span>Pick your destination</h2>
+            <div className='containerBox md:mt-14'>
+                  <img src={images.png} alt={name} title={name} className="img" />
+                <div className='infoBox'>  
+                    <ul className="flex items-center justify-center mb-8 md:justify-start">
+                        {records.map((menu, index) => (
+                          <li key={index} className="mr-7 md:mr-8">
+                            <button onClick={() => setIndexx(index)} 
+                                    className={`font-barlowCondensed not-italic font-normal leading-4 text-specialBlue uppercase text-md tracking-widest pb-2 focus:text-white md:text-md ${index === indexx && "border-b-2 border-white"}`}
+                            >
+                              {menu.name}
+                            </button>
+                          </li>
+                        ))}
+                    </ul>
+                    <h2 className="title">{name}</h2>
+                    <p className= "description">{description}</p>
+                    <ul className="stats-container">
+                      <li className="stats-design">
+                        Avg. Distance
+                        <span className="stats-results">{distance}</span>
+                      </li>
+                      <li className="stats-design">
+                        Est. travel time
+                        <span className="stats-results">{travel}</span>
+                      </li>
+                    </ul>
+                </div>
+            </div>
+       </div>
     </section>
   )
 }
