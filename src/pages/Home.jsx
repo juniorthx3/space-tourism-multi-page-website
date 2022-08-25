9fd221ecdd5f271692c4dc353e171e0074ce3498
Junior MILIEN <juniorthx@gmail.com>
2022-08-25 15:46:31 -0800

    FIX

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,11 +9,7 @@ const Home = () => {
                   <h2 className='heading-2'>
                     So, you want to travel to
                   </h2>
-                  <h4 className='heading-1' 
-                      style={{lineHeight:'172px'}}
-                  >
-                        Space
-                  </h4>
+                  <h4 className='heading-1' style={{lineHeight:'172px'}}>Space</h4>
                   <p className='text'>
                       Let's face it; if you want to go to space, you might as well genuinely go to 
                       outer space and not hover kind of on the edge of it. Well sit back, and relax 
