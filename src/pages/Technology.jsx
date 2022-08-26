46e62ff7b9aa09c6fab06d2839f03c5e34d66572
Junior MILIEN <juniorthx@gmail.com>
2022-08-25 22:39:23 -0400

    OPTIMIZATION II

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -10,21 +10,19 @@ const Technology = () => {
     <section className="technologyBackground technology">
       <div className='mt-28 md:mt-36'>
         <h2 className="h2 mb-3 md:mb-0 md:mx-32"><span className="span">03</span>Space Launch 101</h2>
-        <div className='flex flex-col-reverse justify-center items-center md:flex-row md:px-32'>
-          <div className='text-center md:text-left font-bellefair not-italic font-normal md:flex'>
-                  <ul className="flex justify-center items-center md:flex-col py-5 md:py-0 md:space-y-8">
+        <div className='techBox'>
+          <div className='btnInfo'>
+                  <ul className="boxBtn">
                       {records.map((menu, index) => (
-                          <li key={index} className="font-bellefair font-normal text-white mr-4 md:mr-14 text-xl">
-                              <button onClick={() => setIndexx(index)} 
-                                      className={`pt-1 h-12 w-12  border border-white border-opacity-25 rounded-full
-                                                  ${index === indexx && "bg-white text-black"}`}>
+                          <li key={index} className="list">
+                              <button onClick={() => setIndexx(index)} className={`btnDesign ${index === indexx && "bg-white text-black"}`}>
                                   {index + 1}  
                               </button>
                           </li>
                       ))}
                   </ul>
                   <div>
-                      <h3 className='text-lg md:text-lg font-barlowCondensed text-specialBlue uppercase mix-blend-normal opacity-50'>The Terminology...</h3>
+                      <h3 className='heading-5'>The Terminology...</h3>
                       <h2 className="heading-4">{name}</h2>
                       <p className='text1'>{description}</p>
                   </div>
