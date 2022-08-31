0565d56d8152cfd3335062ed0258ae891121c18e
Junior MILIEN <juniorthx@gmail.com>
2022-08-31 13:56:04 -0800

    FIXING DESTINATION

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -8,16 +8,16 @@ const Destination = () => {
 
   return (
     <section className="destinationBackground destination">
-       <div className='mt-88 md:mt-36'>
-            <h2 className="h2 mb-3 md:mb-0 md:mx-32"><span className="span">01</span>Pick your destination</h2>
-            <div className='containerBox md:mt-12 xl:max-w-7xl xl:mx-auto'>
-                <img src={images.png} alt={name} title={name} className="img hover:animate-pulse" />
+       <div className='mt-88 md:mt-136 lg:mt-150'>
+            <h2 className="h2 mb-3 md:mb-0 md:mx-38 lg:mb-0 lg:mx-32"><span className="span">01</span>Pick your destination</h2>
+            <div className='containerBox md:mt-8 lg:mt-12 xl:max-w-7xl xl:mx-auto'>
+                <img src={images.png} alt={name} title={name} className="img" />
                 <div className='infoBox'>  
                     <ul className="flex items-center justify-center mb-5 lg:justify-start">
                         {records.map((menu, indicator) => (
-                          <li key={indicator} className="px-3 md:mr-8">
+                          <li key={indicator} className="px-3 md:mr-8 lg:mr-2">
                             <button onClick={() => setIndex(indicator)} 
-                                    className={`font-barlowCondensed font-normal text-sm leading-4 tracking-two3 text-specialBlue uppercase pb-2 focus:text-white md:text-md hover:border-b-2 hover:border-solid hover:border-gray-400 ${indicator === index && "border-b-3 border-white"}`}
+                                    className={`font-barlowCondensed font-normal text-sm leading-4 tracking-two3  text-specialBlue uppercase pb-2 md:text-base md:leading-19 md:tracking-two7 focus:text-white md:text-md lg:text-base hover:border-b-2 hover:border-solid hover:border-gray-400 ${indicator === index && "border-b-3 border-white"}`}
                             >
                               {menu.name}
                             </button>
